c59e5c52c6d4af00e7f72e47b1b85af1a2c0a78d
Travis <travisjb0@gmail.com>
2023-09-16 16:43:15 -0700

    Displaying Data

--- reactTesting/src/Component1.jsx
+++ reactTesting/src/Component1.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Component1 = () => {
+    const user ={
+        name: 'Hedy Lamarr',
+        imageUrl: 'https://1fid.com/wp-content/uploads/2022/07/girl-anime-wallaper-25.jpg',
+        imageSize: 90
+    }
+
+
+
   return (
     <div>
+        <h1>{user.name}</h1>
       <button>I'm a button</button>
+      <img 
+        className='avatar' 
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style = {{
+            width: user.imageSize,
+            height: user.imageSize
+        }}
+      />
     </div>
   )
 }
