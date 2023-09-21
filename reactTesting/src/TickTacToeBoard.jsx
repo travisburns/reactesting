83a315347df2703ae3dd1669e43704c3dec88178
Travis <travisjb0@gmail.com>
2023-09-21 15:20:29 -0700

    Lifting state up

--- reactTesting/src/TickTacToeBoard.jsx
+++ reactTesting/src/TickTacToeBoard.jsx
@@ -2,47 +2,49 @@ import React from 'react'
 import {useState} from 'react';
 
 
-const Square = () => {
-  const [value, setValue] = useState(null)  
+const Square = ({value, onSquareClick}) => {
   
   
-  function handleClick() {
-    setValue('X');
-  }
+  
+ 
   
   return  (
-    <button 
-    onClick={handleClick} 
-    className='square'
-    >
-      {value}
-   </button>  
+    <button className='square' onClick={onSquareClick}>{value} </button>  
+
+  
+
       )
  
-
 }
 
 
 const TickTacToeBoard = () => {  
   const [squares, setSquares] = useState(Array(9).fill(null));
   
-  
+  const handleClick = (i) => {
+    const nextSquares = squares.slice()
+    nextSquares[i] = "X"
+    setSquares(nextSquares)
+  }
+
+
+
     return (
         <>
         <div className='Board-row'>
-        <Square />
-        <Square  />   
-        <Square />        
+        <Square  value={squares[0]} onSquareClick= {() => handleClick(0)} />
+        <Square  value={squares[1]} onSquareClick= {() => handleClick(1)}/>   
+        <Square  value={squares[2]} onSquareClick= {() => handleClick(2)}/>        
         </div>  
         <div className='Board-row'>
-        <Square />
-        <Square />
-        <Square />    
+        <Square  value={squares[3]} onSquareClick= {() => handleClick(3)}/>
+        <Square  value={squares[4]} onSquareClick= {() => handleClick(4)}/>
+        <Square  value={squares[5]} onSquareClick= {() => handleClick(5)}/>    
         </div>  
         <div className='Board-row'>
-        <Square />
-        <Square />
-        <Square />    
+        <Square  value={squares[6]} onSquareClick= {() => handleClick(6)}/>
+        <Square  value={squares[7]} onSquareClick= {() => handleClick(7) }/>
+        <Square  value={squares[8]} onSquareClick= {() => handleClick(8)}/>    
         </div>  
             
         </>
