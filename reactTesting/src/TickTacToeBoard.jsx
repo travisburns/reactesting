d15486365a5d1389611c5cde294486db9a4aa816
Travis <travisjb0@gmail.com>
2023-09-21 15:50:48 -0700

    Created Declaring a winnner

--- reactTesting/src/TickTacToeBoard.jsx
+++ reactTesting/src/TickTacToeBoard.jsx
@@ -21,9 +21,44 @@ const Square = ({value, onSquareClick}) => {
 const TickTacToeBoard = () => {
   const [xIsNext, setXIsNext] = useState(true);  
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const calculateWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+
+    return null
+  }
+
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "0");
+  }
+
+
+
   
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -44,6 +79,7 @@ const TickTacToeBoard = () => {
 
     return (
         <>
+        <div className='status'>{status}</div>
         <div className='Board-row'>
         <Square  value={squares[0]} onSquareClick= {() => handleClick(0)} />
         <Square  value={squares[1]} onSquareClick= {() => handleClick(1)}/>   
