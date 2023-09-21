002244fa86ee4c5d427a656978f597598385044b
Travis <travisjb0@gmail.com>
2023-09-21 15:36:44 -0700

    Adding Next turns ability that switches from X to O

--- reactTesting/src/TickTacToeBoard.jsx
+++ reactTesting/src/TickTacToeBoard.jsx
@@ -18,13 +18,26 @@ const Square = ({value, onSquareClick}) => {
 }
 
 
-const TickTacToeBoard = () => {  
+const TickTacToeBoard = () => {
+  const [xIsNext, setXIsNext] = useState(true);  
   const [squares, setSquares] = useState(Array(9).fill(null));
   
   const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice()
-    nextSquares[i] = "X"
+    if  (xIsNext) {
+      nextSquares[i] = "X"
+    } else {
+      nextSquares[i] ="O"
+    }
     setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+
+    
+    
   }
 
 
